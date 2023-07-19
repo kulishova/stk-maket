3b70e9f7d32c3756b70c356d8d8696e91bdc729d
Dari <pinobobino@gmail.com>
2023-07-19 11:05:31 +0300

    Исправления

--- gallery/gallery.js
+++
@@ -1,7 +0,0 @@
-// Инициализация Masonry
-var masonry = new Masonry('#masonry-container', {
-  // Опции и настройки Masonry
-  itemSelector: '.masonry-item', // CSS-селектор для элементов сетки
-  columnWidth: '.masonry-sizer', // CSS-селектор для элемента, определяющего ширину столбцов
-  gutter: 20, // Отступы между элементами
-})
--- swiper/swiper.js
+++ swiper/swiper.js
@@ -6,6 +6,10 @@ const swiper = new Swiper('.swiper', {
   autoplay: {
     delay: 4000,
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
   // If we need pagination
   pagination: {
